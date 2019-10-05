42180a0196d9f0c0b0ccb2a51405dc46b6b35e6e
Pham Hung <phamhung.pitit@gmail.com>
2019-10-05 10:08:47 +0700

    start project win form csdlpt

---
+++ QLSV/QLSV/ptit.qlsv.ui/account/Login.Designer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace QLSV
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbKhoa = new System.Windows.Forms.ComboBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmbKhoa
+            // 
+            this.cmbKhoa.FormattingEnabled = true;
+            this.cmbKhoa.Location = new System.Drawing.Point(194, 110);
+            this.cmbKhoa.Name = "cmbKhoa";
+            this.cmbKhoa.Size = new System.Drawing.Size(337, 21);
+            this.cmbKhoa.TabIndex = 0;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(194, 200);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(337, 20);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(194, 284);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(337, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 478);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.cmbKhoa);
+            this.Name = "Login";
+            this.Text = "Welcome";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbKhoa;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
---
+++ QLSV/QLSV/ptit.qlsv.ui/account/Login.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLSV
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDangNhap_Load(object sender, EventArgs e)
+        {
+            string chuoiketnoi = "Data Source=JAXZ;Initial Catalog=QLSV;Integrated Security=True";
+            Program.conn.ConnectionString = chuoiketnoi;
+            Program.conn.Open();
+            DataTable dt = new DataTable();
+            dt = Program.ExecSqlDataTable("SELECT * FROM V_DSPM");
+            Program.bds_dspm.DataSource = dt;
+            cmbKhoa.DataSource = dt;
+            cmbKhoa.DisplayMember = "TENPM";
+            cmbKhoa.ValueMember = "TENSERVER";
+            cmbKhoa.SelectedIndex = -1;
+
+        }
+
+        private void cmbKhoa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Program.servername = cmbKhoa.SelectedValue.ToString();
+
+            }
+            catch (Exception) { };
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text.Trim() == "" || txtPassword.Text.Trim() == "")
+            {
+                MessageBox.Show("Login name và mật mã không được trống", "", MessageBoxButtons.OK);
+                return;
+            }
+            Program.mlogin = txtUsername.Text; Program.password = txtPassword.Text;
+            if (Program.KetNoi() == 0) return;
+
+            Program.mChinhanh = cmbKhoa.SelectedIndex;
+
+            Program.mloginDN = Program.mlogin;
+            Program.passwordDN = Program.password;
+            string strLenh = "EXEC SP_THONGTINDANGNHAP '" + Program.mlogin + "'";
+
+            Program.myReader = Program.ExecSqlDataReader(strLenh);
+            if (Program.myReader == null) return;
+            Program.myReader.Read();
+
+
+            Program.username = Program.myReader.GetString(0);     // Lay user name
+            if (Convert.IsDBNull(Program.username))
+            {
+                MessageBox.Show("Login bạn nhập không có quyền truy cập dữ liệu\n Bạn xem lại username, password", "", MessageBoxButtons.OK);
+                return;
+            }
+            Program.mHoten = Program.myReader.GetString(1);
+            Program.mGroup = Program.myReader.GetString(2);
+            Program.myReader.Close();
+            Program.conn.Close();
+            MessageBox.Show("Nhan vien - Nhom : " + Program.mHoten + " - " + Program.mGroup, "", MessageBoxButtons.OK);
+
+        }
+
+    }
+}
