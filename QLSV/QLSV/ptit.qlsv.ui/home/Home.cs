e9e7f0de8562ce8fb9baaf7e613ff4f9ba286267
LeTrucLam <letruclamptit@gmail.com>
2019-11-14 17:33:02 +0700

    Done Class

--- QLSV/QLSV/ptit.qlsv.ui/home/Home.cs
+++ QLSV/QLSV/ptit.qlsv.ui/home/Home.cs
@@ -19,12 +19,45 @@ namespace QLSV.ptit.qlsv.ui.home
 
         private void Home_Load(object sender, EventArgs e)
         {
+            cbbKHOA.DataSource = Program.bds_dspm;//sao chep bds
+            cbbKHOA.DisplayMember = "TENKHOA";
+            cbbKHOA.ValueMember = "TENSERVER";
+            cbbKHOA.SelectedIndex = Program.mChinhanh;
+            cbbKHOA.Enabled = true;
 
         }
 
-        private void barButtonItem3_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        private void btnPoint_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
         {
 
+           
+
         }
+
+        private Form CheckExists(Type ftype)
+        {
+            foreach (Form f in this.MdiChildren)
+                if (f.GetType() == ftype)
+                    return f;
+            return null;
+        }
+
+
+        private void btnClass_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+            Form fm = this.CheckExists(typeof(Class));
+            if(fm == null)
+            {
+                Class classForm = new Class();
+                classForm.MdiParent = this;
+                classForm.Show();
+            } else
+            {
+                fm.Activate();
+            }
+        }
+
+       
     }
 }
