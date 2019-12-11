73a46acc9307489856b4aa54cb85e61ecac63b87
Pham Hung <phamhung.pitit@gmail.com>
2019-12-11 23:31:53 +0700

    update

---
+++ QLSV/QLSV/ptit.qlsv.service/students/StudentReport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace QLSV.ptit.qlsv.service.students
+{
+    public partial class StudentReport : DevExpress.XtraEditors.XtraForm
+    {
+        public StudentReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
